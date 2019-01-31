22198cfaf778bed727c106d7963ce8d143bc2ee0
Callum Morham <35584262+callummorham@users.noreply.github.com>
2019-01-31 00:55:04 -0800

    Add files via upload
    up through step 10

---
+++ JS/functions.js
@@ -0,0 +1,4 @@
+//Simple function that returns the added value of two numbers
+function add2(x,y)
+{return x+y;}
+module.exports = add2;
---
+++ JS/index.js
@@ -0,0 +1,4 @@
+//Callum Morham
+//Given two values adds them together then prints the answer
+var total = add2(64,75);
+document.getElementById("header1").innerHTML = "Hello World. 64+75 is:" + total;
