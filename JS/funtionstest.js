ca283bc7eedb287d7acbf0173ff5d7437a07b0c1
Callum Morham <morham20@up.edu>
2019-01-31 01:24:09 -0800

    Delete funtionstest.js

--- JS/funtionstest.js
+++
@@ -1,9 +0,0 @@
-//Simple test functions
-const functions = require('./functions');
-test('adds 1 + 2 to equal 3', () => {
-  expect(functions(1, 2)).toBe(3);
-});
-
-test('object assignment', () => {
-    expect(functions(1,2)).toEqual(3);
-  });
